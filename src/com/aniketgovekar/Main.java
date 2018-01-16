ebbb046846910a090c3959e7f5c5917db4a271f3
Aniket Govekar <aniket.govekar@gmail.com>
2018-01-17 03:07:17 +0530

    Char Boolean Example Complete - 17/01/2018

--- src/com/aniketgovekar/Main.java
+++ src/com/aniketgovekar/Main.java
@@ -3,16 +3,13 @@ package com.aniketgovekar;
 public class Main {
 
     public static void main(String[] args) {
-	// todo Byte Short Int Example
+	// todo Char Boolean Example
 
-        byte myByteValue = 10;
-        short myShortValue = 20;
-        int myIntValue = 50;
-        long myLongValue = 50_000L + (10L * (myByteValue + myShortValue + myIntValue));
-        short myNewShortValue = (short) (50_000 + (10 * (myByteValue + myShortValue + myIntValue)));
-
-        System.out.println("myLongValue = " + myLongValue);
-        System.out.println("myNewShortValue = " + myNewShortValue);
+        //width of 16 (2 bytes)
+        char myChar = '\u00A9';
+        char charSymbol = '\u00AE';
+        System.out.println("charSymbol------>" + charSymbol);
+        System.out.println("myChar-------> " + myChar);
 
     }
 }
