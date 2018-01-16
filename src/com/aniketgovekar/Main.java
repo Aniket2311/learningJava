92c868e237181fe75c183c865e0181b6fb486cb3
Aniket Govekar <aniket.govekar@gmail.com>
2018-01-17 03:05:31 +0530

    Byte Short Int Example Complete - 17/01/2018

--- src/com/aniketgovekar/Main.java
+++ src/com/aniketgovekar/Main.java
@@ -3,6 +3,16 @@ package com.aniketgovekar;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// todo Byte Short Int Example
+
+        byte myByteValue = 10;
+        short myShortValue = 20;
+        int myIntValue = 50;
+        long myLongValue = 50_000L + (10L * (myByteValue + myShortValue + myIntValue));
+        short myNewShortValue = (short) (50_000 + (10 * (myByteValue + myShortValue + myIntValue)));
+
+        System.out.println("myLongValue = " + myLongValue);
+        System.out.println("myNewShortValue = " + myNewShortValue);
+
     }
 }
