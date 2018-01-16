6f6770e7881fb9ac56cb9dab02067e46986e5dd1
Aniket Govekar <aniket.govekar@gmail.com>
2018-01-17 03:08:45 +0530

    Float Double Example Complete - 17/01/2018

--- src/com/aniketgovekar/Main.java
+++ src/com/aniketgovekar/Main.java
@@ -3,13 +3,24 @@ package com.aniketgovekar;
 public class Main {
 
     public static void main(String[] args) {
-	// todo Char Boolean Example
+	// todo Float Double Example
 
-        //width of 16 (2 bytes)
-        char myChar = '\u00A9';
-        char charSymbol = '\u00AE';
-        System.out.println("charSymbol------>" + charSymbol);
-        System.out.println("myChar-------> " + myChar);
+        // width of int = 32 (4bytes)
+        int myIntValue = 5 / 3;
+        // width of float = 32 (4bytes)
+        float myFloatValue = 5f/3f;
+        // width of double = 64 (8bytes)
+        double myDoubleValue = 5d/3d;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+        System.out.println("Challenge--------------");
+
+        double doublePound = 100d;
+        double doubleKgs = doublePound*0.45359237d;
+
+        System.out.println("doubleKgs = " + doubleKgs);
 
     }
 }
