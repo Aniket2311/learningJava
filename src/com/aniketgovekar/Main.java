3fd3b957882efdf6b708fc74ce5cab5a99ce74a1
Aniket Govekar <aniket.govekar@gmail.com>
2018-01-17 03:09:55 +0530

    Hello World Example Complete - 17/01/2018

--- src/com/aniketgovekar/Main.java
+++ src/com/aniketgovekar/Main.java
@@ -3,24 +3,17 @@ package com.aniketgovekar;
 public class Main {
 
     public static void main(String[] args) {
-	// todo Float Double Example
+	// todo Hello World Example
 
-        // width of int = 32 (4bytes)
-        int myIntValue = 5 / 3;
-        // width of float = 32 (4bytes)
-        float myFloatValue = 5f/3f;
-        // width of double = 64 (8bytes)
-        double myDoubleValue = 5d/3d;
+        System.out.println("Hello Aniket!");
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThridNumber = myFirstNumber * 2;
 
-        System.out.println("myIntValue = " + myIntValue);
-        System.out.println("myFloatValue = " + myFloatValue);
-        System.out.println("myDoubleValue = " + myDoubleValue);
-        System.out.println("Challenge--------------");
+        int myTotal = myFirstNumber + mySecondNumber + myThridNumber;
 
-        double doublePound = 100d;
-        double doubleKgs = doublePound*0.45359237d;
-
-        System.out.println("doubleKgs = " + doubleKgs);
+        int difference = 1000 - myTotal;
+        System.out.println(difference);
 
     }
 }
