e93d31a14f1ba10460cdb82c796fb39772bbda50
Aniket Govekar <aniket.govekar@gmail.com>
2018-01-20 20:39:07 +0530

    Operators Part 2 Done - 20/01/2018

--- src/com/aniketgovekar/com/aniketgovekar/operators/Main.java
+++ src/com/aniketgovekar/com/aniketgovekar/operators/Main.java
@@ -37,20 +37,20 @@ public class Main {
         result--;
         System.out.println("result=========> " + result);
 
-        result+=2;
+        result += 2;
         System.out.println("result=========> " + result);
 
-        result*=10;
+        result *= 10;
         System.out.println("result=========> " + result);
 
-        result-=10;
+        result -= 10;
         System.out.println("result=========> " + result);
 
-        result/=10;
+        result /= 10;
         System.out.println("result=========> " + result);
 
         boolean isAlien = false;
-        if(isAlien == true){
+        if (isAlien == true) {
             System.out.println("It is not an Alien !!!!");
         }
 
@@ -59,7 +59,36 @@ public class Main {
             System.out.println("You got the highest score !!!");
 
         int secondTopScore = 81;
-        if(topScore > secondTopScore && topScore < 100)
+        if ((topScore > secondTopScore) && (topScore < 100))
             System.out.println("Greater than secondTopScore and less than 100");
+
+        if ((topScore > 90) || (secondTopScore <= 90))
+            System.out.println("One of these tests is true !!!!");
+
+        int newValue = 50;
+        if (newValue == 50)
+            System.out.println("This is no longer an Error.......");
+
+
+        boolean isChar = false;
+        if (isChar)
+            System.out.println("This is not to happen.......");
+
+        boolean wasChar = isChar ? true : false;
+        System.out.println("wasChar========> " + wasChar);
+
+        //todo Challenge
+
+        double doubltFirstValue = 20d;
+        double doubltSecondValue = 80d;
+        double doubleResult = (doubltFirstValue + doubltSecondValue) * 25;
+        System.out.println("doubleResult======> " + doubleResult);
+        double doubleRemainder = doubleResult % 40;
+        System.out.println("doubleRemainder======> " + doubleRemainder);
+
+        if (doubleRemainder <= 20)
+            System.out.println("Total was over the limit !!!!!");
+
+
     }
 }
