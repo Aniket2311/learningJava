bc44bcec4e12654c7c171b237ebbf947f3353849
Aniket Govekar <aniket.govekar@gmail.com>
2018-01-23 01:43:49 +0530

    WhiteSpace And Indentation Done - 23/01/2018

---
+++ src/com/aniketgovekar/com/aniketgovekar/operators/com/aniketgovekar/whitespaceandindenting/Main.java
@@ -0,0 +1,15 @@
+package com.aniketgovekar.com.aniketgovekar.operators.com.aniketgovekar.whitespaceandindenting;
+
+public class Main {
+    public static void main(String[] args) {
+        int myValue = 50;
+        myValue++;
+        myValue--;
+        System.out.println("This is a test");
+        System.out.println("This is " +
+                " another" +
+                " still more");
+
+        int anotherVariable = 50;anotherVariable--;System.out.println("This is the end");
+    }
+}
