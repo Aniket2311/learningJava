814a3ae4d91b7e5c48404851627e574dfc00ed87
Aniket Govekar <aniket.govekar@gmail.com>
2018-01-23 01:31:37 +0530

    Keywords And Expressions Done - 23/01/2018

---
+++ src/com/aniketgovekar/com/aniketgovekar/operators/com/aniketgovekar/keywordsandexpressions/Main.java
@@ -0,0 +1,21 @@
+package com.aniketgovekar.com.aniketgovekar.operators.com.aniketgovekar.keywordsandexpressions;
+
+public class Main {
+
+    public static void main(String[] args){
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+        if(highScore == 50){
+            System.out.println("This is an expression !!!!");
+        }
+
+        int score = 100; // score = 100 is expression
+        if(score > 99){ // score > 99 is expression
+            System.out.println("You got the high score..."); // You got the high score is an expression
+            score = 0; // score = 0 is an expression
+        }
+
+
+
+    }
+}
