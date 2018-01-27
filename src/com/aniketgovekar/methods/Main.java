06979fa2142e83ced600bd7f28330c541f773637
Aniket Govekar <aniket.govekar@gmail.com>
2018-01-27 17:49:32 +0530

    Methos Part 2 Done - 27/01/2018

--- src/com/aniketgovekar/methods/Main.java
+++ src/com/aniketgovekar/methods/Main.java
@@ -14,20 +14,43 @@ public class Main {
         levelCompleted = 8;
         bonus = 200;
 
-        finalScore = calculateScore(gameOver,score,levelCompleted,bonus);
+        finalScore = calculateScore(gameOver, score, levelCompleted, bonus);
         System.out.println("finalScore2------------> " + finalScore);
+
+        //////////todo Challenge
+        displayHighScorePosition("Player1",calculateHighScorePosition(1000));
+        displayHighScorePosition("Player2",calculateHighScorePosition(500));
+        displayHighScorePosition("Player3",calculateHighScorePosition(100));
+        displayHighScorePosition("Player4",calculateHighScorePosition(50));
+
 
     }
 
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("finalScore----------> " + finalScore);
             return finalScore;
         } else {
             return -1;
+        }
+    }
+
+    public static void displayHighScorePosition(String strPlayerName, int intPosition) {
+        System.out.println(strPlayerName + " managed to get into position " + intPosition + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int intPlayerScore) {
+
+        if (intPlayerScore >= 1000) {
+            return 1;
+        } else if (intPlayerScore >= 500 && intPlayerScore < 1000) {
+            return 2;
+        } else if (intPlayerScore >= 100 && intPlayerScore < 500) {
+            return 3;
+        } else {
+            return 4;
         }
     }
 }
