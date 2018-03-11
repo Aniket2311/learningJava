1be7c0d5215411940476410d699c9d36465d0326
Aniket Govekar <aniket.govekar@gmail.com>
2018-03-11 12:14:27 +0530

    OOP Test Part 2 Done - 11/03/2018

--- src/com/aniketgovekar/ooptest/Main.java
+++ src/com/aniketgovekar/ooptest/Main.java
@@ -7,7 +7,14 @@ public class Main {
         hamburger.addHamburgerAddition2("Lettuce", 0.75);
         hamburger.addHamburgerAddition3("Cheese", 1.12);
 
-        double price = hamburger.itemizeHamburger();
-        System.out.println("Total comes to " + price);
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthAddition1("Lentils", 3.41);
+        System.out.println("Total price -> " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger deluxeBurger = new DeluxeBurger();
+        deluxeBurger.addHamburgerAddition1("Should not do this" , 50.53);
+        System.out.println("Total price for Deluxe Burger -> " + deluxeBurger.itemizeHamburger());
+
     }
 }
