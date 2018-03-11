16f8bed3bbee2ca864649457e41245b92b027c52
Aniket Govekar <aniket.govekar@gmail.com>
2018-03-11 11:45:26 +0530

    OOP Test Part 1 Done - 11/03/2018

---
+++ src/com/aniketgovekar/ooptest/Main.java
@@ -0,0 +1,13 @@
+package com.aniketgovekar.ooptest;
+
+public class Main {
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.12);
+
+        double price = hamburger.itemizeHamburger();
+        System.out.println("Total comes to " + price);
+    }
+}
