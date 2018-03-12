488ebb03e7ccc67b35be93d767711aadfcec03de
Aniket Govekar <aniket.govekar@gmail.com>
2018-03-12 22:53:51 +0530

    Arrays Part 2 Done - 12/03/2018

--- src/com/aniketgovekar/arrays/Main.java
+++ src/com/aniketgovekar/arrays/Main.java
@@ -1,21 +1,37 @@
 package com.aniketgovekar.arrays;
 
-public class Main {
-    public static void main(String[] args) {
-        int[] intArray = new int[25];
+import java.util.Scanner;
 
-        //////////////////todo Using for loop
-        for (int i=0; i<intArray.length; i++){
-            intArray[i]=i*10;
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+        for (int i = 0; i < myIntegers.length; i++) {
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
         }
 
-        printArray(intArray);
+        System.out.println("The average is " + getAverage(myIntegers));
     }
 
-    public static void printArray(int[] intArray){
-        for (int i=0; i<intArray.length; i++){
-            System.out.println(intArray[i]);
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
         }
 
+        return values;
+    }
+
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+
+        return (double) sum / (double) array.length;
     }
 }
