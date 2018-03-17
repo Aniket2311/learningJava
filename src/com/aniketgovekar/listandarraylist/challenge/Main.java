e7d4e3079e9a34ea8707a9c2fb1facd13e7232cd
Aniket Govekar <aniket.govekar@gmail.com>
2018-03-17 19:45:09 +0530

    ArrayList Part 4 - Challenge Part 2 - Done - 17/03/2018

--- src/com/aniketgovekar/listandarraylist/challenge/Main.java
+++ src/com/aniketgovekar/listandarraylist/challenge/Main.java
@@ -1,7 +1,31 @@
 package com.aniketgovekar.listandarraylist.challenge;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        boolean quit = false;
+        startPhone();
+        printAction();
+
+        while (!quit) {
+
+        }
+    }
+
+    private static void printAction() {
+        System.out.println("\nAvailable actions:\npress");
+        System.out.println("0 - to shutdown\n" +
+                "1 - to print contacts\n" +
+                "2 - to add a contact\n" +
+                "3 - to update an existing contact\n" +
+                "4 - to remove an existing contact\n" +
+                "5 - query if an existing contact exists\n" +
+                "6 - to print a list of available actions.\n"
+        );
+        System.out.println("Choose your action: ");
+    }
+
+    private static void startPhone() {
+        System.out.println("Starting phone........");
     }
 }
