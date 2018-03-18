28888ab0b8a90b109babf6d874739d4d932f97e4
Aniket Govekar <aniket.govekar@gmail.com>
2018-03-18 18:14:35 +0530

    AutoBoxing and Unboxing - Challenge Part 3 - Done - 18/03/2018

--- src/com/aniketgovekar/autoboxingandunboxing/challenge/Main.java
+++ src/com/aniketgovekar/autoboxingandunboxing/challenge/Main.java
@@ -2,6 +2,25 @@ package com.aniketgovekar.autoboxingandunboxing.challenge;
 
 public class Main {
     public static void main(String[] args) {
+        Bank bank = new Bank("HDFC");
 
+        bank.addBranch("Borivali");
+
+        bank.addCustomer("Borivali", "Aniket", 10_000.00);
+        bank.addCustomer("Borivali", "Shivaji", 10_000.00);
+        bank.addCustomer("Borivali", "Shilpa", 10_000.00);
+
+        bank.addBranch("Kandivali");
+        bank.addCustomer("Kandivali", "Mahadev", 10_000.00);
+
+        bank.addCustomerTransaction("Borivali", "Aniket", 20_000.00);
+        bank.addCustomerTransaction("Borivali", "Shivaji", 10_000.00);
+        bank.addCustomerTransaction("Borivali", "Shilpa", 5_000.00);
+        bank.addCustomerTransaction("Kandivali", "Mahadev", 50_000.00);
+
+        bank.listCustomers("Borivali", false);
+        bank.listCustomers("Kandivali", false);
+        bank.listCustomers("Borivali", true);
+        bank.listCustomers("Kandivali", true);
     }
 }
