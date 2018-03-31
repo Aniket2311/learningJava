7b08bd0793aa358d139bfd15205a0a009a78eedd
Aniket Govekar <user14@sunrichtech.com>
2018-03-31 16:25:41 +0530

    Generics Part 5 - Challenge - Done - 31/03/2018

---
+++ src/com/aniketgovekar/generics/challenge/BaseballPlayer.java
@@ -0,0 +1,12 @@
+package com.aniketgovekar.generics.challenge;
+
+import com.aniketgovekar.generics.Player;
+
+/**
+ * Created by admin on 30-03-2018.
+ */
+public class BaseballPlayer extends Player {
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
